9a804698c0cb7cf16c141ffdd2ddb00db32c62fe
chenweijie <1729158008@qq.com>
2022-05-23 01:04:01 +0800

    Create README.md

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author 17291
+ * @version 1.0
+ * @className Test
+ * @description TODO 测试用类
+ * @date 2022/5/23 1:07
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
