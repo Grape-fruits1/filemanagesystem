e67f59f19f009dda5eb90a6ab622326508e46ead
lodisaq <lodisaq@163.com>
2022-05-23 13:15:31 +0800

    Changes

--- src/Test.java
+++ src/Test.java
@@ -7,6 +7,8 @@
  */
 public class Test {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("114514");
         System.out.println(1);
     }
 }
