d08eb970306ebc7bc97e46ec17ff1bc560214641
chenweijie <1729158008@qq.com>
2022-06-05 05:36:13 +0800

    增加了文件控制块中的相关控制信息

---
+++ src/User.java
@@ -0,0 +1,14 @@
+/**
+ * @author 17291
+ * @version 1.0
+ * @className User
+ * @description TODO 用户
+ * @date 2022/6/5 2:41
+ */
+public class User {
+    String userName;
+    String password;
+    User () {
+
+    }
+}
